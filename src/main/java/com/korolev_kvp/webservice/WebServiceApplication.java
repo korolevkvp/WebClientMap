c7de0a6c130456e1732ad10ae53edf7ad1fb5b63
Vadim Korolev <vad05082001@gmail.com>
2021-09-16 13:44:40 +0300

    Добавлен Javadoc для класса WebServiceApplication.

--- src/main/java/com/korolev_kvp/webservice/WebServiceApplication.java
+++ src/main/java/com/korolev_kvp/webservice/WebServiceApplication.java
@@ -3,6 +3,9 @@ package com.korolev_kvp.webservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Класс для запуска сервиса.
+ */
 @SpringBootApplication
 public class WebServiceApplication {
 
